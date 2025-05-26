50e8f9cbe4c38c40d82031e86039cda3fb73645b
RaphaelBattistel <raphaelbattistel@gmail.com>
2025-05-26 10:20:44 +0200

    Commit modif scripts usp/ datas

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "Data", order = 1)]
 public class Data : ScriptableObject
 {
-    public Sprite sprite;
+    public Sprite Question;
+    public Sprite Reponse1;
+    public Sprite Reponse2;
+    public Sprite Reponse3;
     public int idQuestion;
     public int idReponse;
 
